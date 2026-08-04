887e606952b83f853655faae5f3a02a1720dfb4a
Vkbarbabalo <Villekihl@hotmail.com>
2026-08-04 21:55:35 +0200

    Update video links in default demon list

--- list-engine.js
+++ list-engine.js
@@ -1,9 +1,9 @@
 const defaultDemonListData = [
-  { rank: 1, name: "Bloodbath", id: "10565740", creator: "Riot", verifier: "Vk_barbabalo", points: 100, video: "https://youtube.com", victors: [] },
-  { rank: 2, name: "Cataclysm", id: "3979721", creator: "Ggb0y", verifier: "Vk_barbabalo", points: 75, video: "https://youtube.com", victors: [] },
-  { rank: 3, name: "Acropolis", id: "5155022", creator: "Zobros", verifier: "Vk_barbabalo", points: 50, video: "", victors: [{ name: "Swedishvic", video: "https://youtube.com" }] },
-  { rank: 4, name: "Oblivion", id: "114755656", creator: "Defentum", verifier: "Vk_barbabalo", points: 30, video: "https://youtube.com", victors: [] },
-  { rank: 5, name: "Nine circles", id: "4284013", creator: "Zobros", verifier: "Vk_barbabalo", points: 20, video: "", victors: [{ name: "Swedishvic", video: "https://outplayed.tv" }] }
+  { rank: 1, name: "Bloodbath", id: "10565740", creator: "Riot", verifier: "Vk_barbabalo", points: 100, video: "https://www.youtube.com/watch?v=Uy92UvhyNl0&t=1s", victors: [] },
+  { rank: 2, name: "Cataclysm", id: "3979721", creator: "Ggb0y", verifier: "Vk_barbabalo", points: 75, video: "https://www.youtube.com/watch?v=dXs_UjKpQUQ&t=2s", victors: [] },
+  { rank: 3, name: "Acropolis", id: "5155022", creator: "Zobros", verifier: "Vk_barbabalo", points: 50, video: "", victors: [{ name: "Swedishvic", video: "https://www.youtube.com/watch?v=fVBmzkqykKE" }] },
+  { rank: 4, name: "Oblivion", id: "114755656", creator: "Defentum", verifier: "Vk_barbabalo", points: 30, video: "https://www.youtube.com/watch?v=2F0VMXlWq4Y", victors: [] },
+  { rank: 5, name: "Nine circles", id: "4284013", creator: "Zobros", verifier: "Vk_barbabalo", points: 20, video: "", victors: [{ name: "Swedishvic", video: "https://outplayed.tv/geometry-dash/K4jm60" }] }
 ];
 
 const coreUIStructure = `
