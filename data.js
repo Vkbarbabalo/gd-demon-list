dcccde1882977708300b399e2ba3496e8a813d07
Vkbarbabalo <Villekihl@hotmail.com>
2026-08-04 20:08:28 +0200

    Add initial demon list data to data.js

---
+++ data.js
@@ -0,0 +1,25 @@
+const demonListData = [
+  {
+    rank: 1,
+    name: "Bloodlust",
+    id: "39474773",
+    creator: "Knobbelboy",
+    verifier: "Knobbelboy",
+    video: "https://youtube.com", // Replace with actual YouTube embed link
+    victors: [
+      { name: "Technical", video: "https://youtube.com" },
+      { name: "SpaceUK", video: "https://youtube.com" }
+    ]
+  },
+  {
+    rank: 2,
+    name: "Sonic Wave",
+    id: "25974015",
+    creator: "Cyclic",
+    verifier: "Sunix",
+    video: "https://youtube.com",
+    victors: [
+      { name: "Mefewe", video: "https://youtube.com" }
+    ]
+  }
+];
