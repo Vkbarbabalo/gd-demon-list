350415568c4987f099fb3176e4254b46d538fb09
Vkbarbabalo <Villekihl@hotmail.com>
2026-08-04 20:27:28 +0200

    Update demon list with new names and details

--- data.js
+++ data.js
@@ -1,25 +1,45 @@
 const demonListData = [
   {
     rank: 1,
-    name: "Bloodlust",
-    id: "39474773",
-    creator: "Knobbelboy",
-    verifier: "Knobbelboy",
-    video: "https://youtube.com", // Replace with actual YouTube embed link
+    name: "Bloodbath",
+    id: "10565740",
+    creator: "Riot",
+    verifier: "Vk_barbabalo",
+    video: "https://www.youtube.com/watch?v=Uy92UvhyNl0", // Replace with actual YouTube embed link
     victors: [
-      { name: "Technical", video: "https://youtube.com" },
-      { name: "SpaceUK", video: "https://youtube.com" }
     ]
   },
   {
     rank: 2,
-    name: "Sonic Wave",
-    id: "25974015",
-    creator: "Cyclic",
-    verifier: "Sunix",
-    video: "https://youtube.com",
+    name: "Cataclysm",
+    id: "3979721",
+    creator: "Ggb0y",
+    verifier: "Vk_barbabalo",
+    video: "https://www.youtube.com/watch?v=dXs_UjKpQUQ",
     victors: [
-      { name: "Mefewe", video: "https://youtube.com" }
+    ]
+  },
+  {
+    rank: 3,
+    name: "Acropolis",
+    id: "5155022",
+    creator: "Zobros",
+    verifier: "Vk_barbabalo",
+    video: "-", // Replace with actual YouTube embed link
+    victors: [
+      { name: "Swedishvic", video: "https://www.youtube.com/watch?v=fVBmzkqykKE" }
+    ]
+  },
+  {
+    rank: 4,
+    name: "Nine circles",
+    id: "4284013",
+    creator: "Zobros",
+    verifier: "Vk_barbabalo",
+    video: "-",
+    victors: [
+      { name: "Swedishvic", video: "https://outplayed.tv/geometry-dash/K4jm60" }
     ]
   }
 ];
+
